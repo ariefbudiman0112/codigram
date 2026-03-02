92f6d97ee89148e56c3d1f2a4db35e2574dad519
ariefbudiman0112 <ariefbudiman0112@gmail.com>
2026-03-02 10:35:44 +0100

    generate models

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # codigram
+
+## models
+
+npx sequelize-cli model:generate --name User --attributes username:string,email:string,password:string,role:string
+
+npx sequelize-cli model:generate --name Profile --attributes fullName:string,bio:text,avatarUrl:text,UserId:integer
+
+npx sequelize-cli model:generate --name Category --attributes name:string,description:text
+
+npx sequelize-cli model:generate --name Post --attributes caption:text,imageUrl:text,UserId:integer,CategoryId:integer
+
+npx sequelize-cli model:generate --name Comment --attributes message:text,UserId:integer,PostId:integer
+
+npx sequelize-cli model:generate --name Like --attributes UserId:integer,PostId:integer
---
+++ src/migrations/20260302093458-create-post.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      caption: {
+        type: Sequelize.TEXT
+      },
+      imageUrl: {
+        type: Sequelize.TEXT
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Posts');
+  }
+};
